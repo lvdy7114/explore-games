cf4f23ce3f464de08f0a2581d9b5071c84114fff
Linda Vadysirisack <linda.vady@gmail.com>
2023-07-27 22:39:31 -0500

    Add accordion for platform and ratings

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,20 +1,46 @@
-import { Card, CardBody, Heading, Image, Text, HStack } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Image, Text, HStack, Accordion,AccordionItem,AccordionButton,AccordionPanel,AccordionIcon } from '@chakra-ui/react'
 import React from 'react'
 
 const GameCard = ({ game }) => {
     const platformNames = game.platforms.map(({ platform }) => platform.name);
+   
 
   return (
     <Card borderRadius={10} overflow='hidden'>
         <Image src={game.background_image} />
       <CardBody>
       <Heading fontSize='2xl'>{game.name}</Heading>
-      <HStack justifyContent='space-between' marginBottom={3}>
-          {/* Display platform names */}
-          <Text fontSize='sm'>{platformNames.join(', ')}</Text>
-          {/* Add any additional content for HStack if needed */}
-        </HStack>
-        
+      <Accordion allowMultiple>
+          {/* Accordion Item for Platforms */}
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Text flex="1" textAlign="left">
+                  Platforms
+                </Text>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Text fontSize="sm">{platformNames.join(', ')}</Text>
+            </AccordionPanel>
+          </AccordionItem>
+          {/* Accordion Item for Description */}
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Text flex="1" textAlign="left">
+                  Ratings
+                </Text>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Text fontSize="sm">Metacritic: {game.metacritic}<br/>
+                                  Rating: {game.rating}</Text>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </CardBody>
     </Card>
   )
